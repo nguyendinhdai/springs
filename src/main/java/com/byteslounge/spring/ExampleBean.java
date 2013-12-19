e2aa4cd48c043b75b8506309cf58b652f6b3fdb5
javaadvanceprogramming@gmail.com <admin123##>
2013-12-19 10:46:51 +0700

    spring example commited

---
+++ src/main/java/com/byteslounge/spring/ExampleBean.java
@@ -0,0 +1,44 @@
+/*
+ * FILENAME
+ *     ExampleBean.java
+ *
+ * FILE LOCATION
+ *     $Source$
+ *
+ * VERSION
+ *     $Id$
+ *         @version       $Revision$
+ *         Check-Out Tag: $Name$
+ *         Locked By:     $Lockers$
+ *
+ * FORMATTING NOTES
+ *     * Lines should be limited to 78 characters.
+ *     * Files should contain no tabs.
+ *     * Indent code using four-character increments.
+ *
+ * COPYRIGHT
+ *     Copyright (C) 2007 Genix Ventures Pty. Ltd. All rights reserved.
+ *     This software is the confidential and proprietary information of
+ *     Genix Ventures ("Confidential Information"). You shall not
+ *     disclose such Confidential Information and shall use it only in
+ *     accordance with the terms of the license agreement you entered into
+ *     with Genix Ventures.
+ */
+
+package com.byteslounge.spring;
+
+//
+// IMPORTS
+// NOTE: Import specific classes without using wildcards.
+//
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExampleBean
+{
+    public String sayHello()
+    {
+        return "Hello!";
+    }
+}
---
+++ src/main/java/com/byteslounge/spring/Main.java
@@ -0,0 +1,46 @@
+/*
+ * FILENAME
+ *     Main.java
+ *
+ * FILE LOCATION
+ *     $Source$
+ *
+ * VERSION
+ *     $Id$
+ *         @version       $Revision$
+ *         Check-Out Tag: $Name$
+ *         Locked By:     $Lockers$
+ *
+ * FORMATTING NOTES
+ *     * Lines should be limited to 78 characters.
+ *     * Files should contain no tabs.
+ *     * Indent code using four-character increments.
+ *
+ * COPYRIGHT
+ *     Copyright (C) 2007 Genix Ventures Pty. Ltd. All rights reserved.
+ *     This software is the confidential and proprietary information of
+ *     Genix Ventures ("Confidential Information"). You shall not
+ *     disclose such Confidential Information and shall use it only in
+ *     accordance with the terms of the license agreement you entered into
+ *     with Genix Ventures.
+ */
+
+package com.byteslounge.spring;
+
+//
+// IMPORTS
+// NOTE: Import specific classes without using wildcards.
+//
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+        ExampleBean exampleBean = (ExampleBean) ctx.getBean("exampleBean");
+        System.out.println(exampleBean.sayHello());
+    }
+}
